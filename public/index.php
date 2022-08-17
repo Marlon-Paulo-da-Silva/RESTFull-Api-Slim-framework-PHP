c3d0e94e5e68269d9125bf69125d936a55a28da5
Marlon Paulo <marlon.paulo.silva@outlook.com>
2022-08-17 13:39:01 -0300

    Fisrt commit

---
+++ public/index.php
@@ -0,0 +1,40 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Psr\Http\Message\MessageInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+
+$app = AppFactory::create();
+
+$app->get('/', function(Request $request, Response $response, $args){
+  $response->getBody()->write('Olá, Marlon');
+  return $response;
+});
+$app->get('/fruits', function(Request $request, Response $response, $args){
+  $fruits = [
+    '1' => 'Banana',
+    '2' => 'Laranja',
+    '3' => 'Abacaxi'
+  ];
+
+  $response->getBody()->write(json_encode($fruits));
+  return $response->withHeader('Content-type', 'application/json');
+});
+
+$app->get('/fruits/{id}', function(Request $request, Response $response, $args){
+  $fruits = [
+    '1' => 'Banana',
+    '2' => 'Laranja',
+    '3' => 'Abacaxi'
+  ];
+
+  $fruit[$args['id']] = $fruits[$args['id']];
+
+  $response->getBody()->write(json_encode($fruit));
+  return $response->withHeader('Content-type', 'application/json');
+});
+
+$app->run();
